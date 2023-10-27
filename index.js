4770b02d3d2cd6c8febaf5eedc95d792e98a2a8d
James Trimble <jtrim.ons@gmail.com>
2023-10-27 21:26:13 +0100

    Try to follow xo style, mostly

--- index.js
+++ index.js
@@ -1,107 +1,113 @@
-function multiply_and_delta_encode(lines, column_types) {
-    for (let {key, col_type} of column_types) {
-        if (typeof col_type !== "number") continue;
-        let prev = 0;
-        for (let line of lines) {
-            if (line[key] == null) continue;
-            let multiplied = Math.round(line[key] * col_type);
-            line[key] = multiplied - prev;
-            prev = multiplied;
-        }
-    }
-}
-
-function get_data_by_column(lines, column_types) {
-    let data_by_column = [];
-    for (let {key, col_type} of column_types) {
-        data_by_column.push({
-            "name": key,
-            "type": typeof col_type === "number" ? "delta" : col_type,
-            "multiplier": typeof col_type === "number" ? col_type : null,
-            "data": lines.map(line => line[key])
-        })
-    }
-    return data_by_column;
-}
-
-export function compressData(lines, column_types) {
-    lines = JSON.parse(JSON.stringify(lines));
-
-    let interned_strings = {};
-    let interned_strings_len = 0;
-
-    for (let line of lines) {
-        for (let {key, col_type} of column_types) {
-            if (col_type === "interned_string") {
-                let s = line[key];
-                if (!(s in interned_strings)) {
-                    interned_strings[s] = interned_strings_len;
-                    interned_strings_len++;
-                }
-                line[key] = interned_strings[s];
-            } else if (col_type === "int" || col_type === "float") {
-                line[key] = +line[key];
-            } else if (col_type !== "string") {
-                if (line[key] === '' || line[key] === null) {
-                    line[key] = null;
-                } else {
-                    line[key] = +line[key];
-                }
-            }
-        }
-    }
-
-    multiply_and_delta_encode(lines, column_types)
-    let data_by_column = get_data_by_column(lines, column_types)
-
-    let interned_strings_arr = Array(interned_strings_len);
-    for (let [s, i] of Object.entries(interned_strings)) {
-        interned_strings_arr[i] = s;
-    }
-
-    return {"interned_strings": interned_strings_arr, "columns": data_by_column};
-}
-
-export function decompressData({interned_strings, columns}, objectFn) {
-    const dummyDataForChecking = columns.map(column => [column.name]);
-    const dataToCheck = objectFn(dummyDataForChecking, 0);
-    if (Object.entries(dataToCheck).length !== columns.length) {
-        throw new Error("Incorrect objectFn: number of fields doesn't match number of columns.")
-    }
-    for (let [key, val] of Object.entries(dataToCheck)) {
-        if (key !== val) {
-            throw new Error(`Incorrect objectFn: "${key}" !== "${val}".`)
-        }
-    }
-
-    const columnData = [];
-
-    for (const column of columns) {
-    	let data;
-        if (column.type === 'delta') {
-            let val = 0;
-            data = Array(column.data.length);
-            for (let i=0; i<column.data.length; i++) {
-            	data[i] = column.data[i] === null ? null : (val += column.data[i]) / column.multiplier;
-			}
-        } else if (column.type === 'interned_string') {
-            data = Array(column.data.length);
-            for (let i=0; i<column.data.length; i++) {
-            	data[i] = interned_strings[column.data[i]];
-			}
-        } else {
-        	data = column.data;
+function multiplyAndDeltaEncode(lines, columnTypes) {
+	for (const {key, colType} of columnTypes) {
+		if (typeof colType !== 'number') {
+			continue;
 		}
-    	columnData.push(data);
-    }
 
-    // d3.dsv has a fancy objectConverter function for speed,
-    // https://github.com/d3/d3-dsv/blob/a2facce660bb4895b56c62a655d0f252efc3d99f/src/dsv.js#L7-L11
-    // but it creates a new function from a string in a way that I'm nervous about
+		let previous = 0;
+		for (const line of lines) {
+			if (line[key] === null) {
+				continue;
+			}
 
-    const decompressedData = columns[0].data.map((_, rowNumber) => objectFn(columnData, rowNumber));
+			const multiplied = Math.round(line[key] * colType);
+			line[key] = multiplied - previous;
+			previous = multiplied;
+		}
+	}
+}
 
-    decompressedData.columns = columns.map(d => d.name);
+function getDataByColumn(lines, columnTypes) {
+	const dataByColumn = [];
+	for (const {key, colType} of columnTypes) {
+		dataByColumn.push({
+			name: key,
+			type: typeof colType === 'number' ? 'delta' : colType,
+			multiplier: typeof colType === 'number' ? colType : null,
+			data: lines.map(line => line[key]),
+		});
+	}
 
-    return decompressedData;
+	return dataByColumn;
+}
+
+export function compressData(lines, columnTypes) {
+	lines = JSON.parse(JSON.stringify(lines));
+
+	const internedStrings = {};
+	let internedStringsLength = 0;
+
+	for (const line of lines) {
+		for (const {key, colType} of columnTypes) {
+			if (colType === 'interned_string') {
+				const s = line[key];
+				if (!(s in internedStrings)) {
+					internedStrings[s] = internedStringsLength;
+					internedStringsLength++;
+				}
+
+				line[key] = internedStrings[s];
+			} else if (colType === 'int' || colType === 'float') {
+				line[key] = Number(line[key]);
+			} else if (colType !== 'string') {
+				line[key] = line[key] === '' || line[key] === null ? null : Number(line[key]);
+			}
+		}
+	}
+
+	multiplyAndDeltaEncode(lines, columnTypes);
+	const dataByColumn = getDataByColumn(lines, columnTypes);
+
+	const internedStringsArray = Array.from({length: internedStringsLength});
+	for (const [s, i] of Object.entries(internedStrings)) {
+		internedStringsArray[i] = s;
+	}
+
+	return {internedStrings: internedStringsArray, columns: dataByColumn};
+}
+
+export function decompressData({internedStrings, columns}, objectFn) {
+	const dummyDataForChecking = columns.map(column => [column.name]);
+	const dataToCheck = objectFn(dummyDataForChecking, 0);
+	if (Object.entries(dataToCheck).length !== columns.length) {
+		throw new Error('Incorrect objectFn: number of fields doesn\'t match number of columns.');
+	}
+
+	for (const [key, value] of Object.entries(dataToCheck)) {
+		if (key !== value) {
+			throw new Error(`Incorrect objectFn: "${key}" !== "${value}".`);
+		}
+	}
+
+	const columnData = [];
+
+	for (const column of columns) {
+		let data;
+		if (column.type === 'delta') {
+			let value = 0;
+			data = new Array(column.data.length);
+			for (let i = 0; i < column.data.length; i++) {
+				data[i] = column.data[i] === null ? null : (value += column.data[i]) / column.multiplier;
+			}
+		} else if (column.type === 'interned_string') {
+			data = new Array(column.data.length);
+			for (let i = 0; i < column.data.length; i++) {
+				data[i] = internedStrings[column.data[i]];
+			}
+		} else {
+			data = column.data;
+		}
+
+		columnData.push(data);
+	}
+
+	// D3.dsv has a fancy objectConverter function for speed,
+	// https://github.com/d3/d3-dsv/blob/a2facce660bb4895b56c62a655d0f252efc3d99f/src/dsv.js#L7-L11
+	// but it creates a new function from a string in a way that I'm nervous about
+
+	const decompressedData = columns[0].data.map((_, rowNumber) => objectFn(columnData, rowNumber));
+
+	decompressedData.columns = columns.map(d => d.name);
+
+	return decompressedData;
 }
--- smaller.js
+++ smaller.js
@@ -1,18 +1,18 @@
-import fs from "fs";
-import { csvParse } from "d3-dsv";
-import { compressData } from './index.js';
+import fs from 'node:fs';
+import {csvParse} from 'd3-dsv';
+import {compressData} from './index.js';
 
 const csvFilename2 = 'oa21-data.csv';
-const fileContent2 = fs.readFileSync(csvFilename2, {encoding: "utf8", flag: "r"});
+const fileContent2 = fs.readFileSync(csvFilename2, {encoding: 'utf8', flag: 'r'});
 const csvLines2 = csvParse(fileContent2);
-let compressed2 = compressData(csvLines2, [
-    {key: 'oa21cd', col_type: 'string'},
-    {key: 'lsoa21cd', col_type: 'string'},
-    {key: 'msoa21cd', col_type: 'string'},
-    {key: 'ltla21cd', col_type: 'string'},
-    {key: 'lng', col_type: 10000},
-    {key: 'lat', col_type: 10000},
-    {key: 'population', col_type: 'int'}
+const compressed2 = compressData(csvLines2, [
+	{key: 'oa21cd', colType: 'string'},
+	{key: 'lsoa21cd', colType: 'string'},
+	{key: 'msoa21cd', colType: 'string'},
+	{key: 'ltla21cd', colType: 'string'},
+	{key: 'lng', colType: 10_000},
+	{key: 'lat', colType: 10_000},
+	{key: 'population', colType: 'int'},
 ]);
-fs.writeFileSync('oa21-data.json', JSON.stringify(compressed2))
+fs.writeFileSync('oa21-data.json', JSON.stringify(compressed2));
 
