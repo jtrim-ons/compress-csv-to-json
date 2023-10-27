19a3416527d86f88829336b0b14bfa2b5052cd2b
James Trimble <jtrim.ons@gmail.com>
2023-10-27 21:06:35 +0100

    Don't modify data passed to decompressData

--- open_json.js
+++ open_json.js
@@ -6,14 +6,14 @@ const startTime = performance.now();
 const compressedFilename = 'oa21-data.json';
 const fileContent = fs.readFileSync(compressedFilename, {encoding: "utf8", flag: "r"});
 const compressedData = JSON.parse(fileContent);
-const decompressedData = decompressData(compressedData, (columns, rowNumber) => ({
-    oa21cd: columns[0].data[rowNumber],
-    lsoa21cd: columns[1].data[rowNumber],
-    msoa21cd: columns[2].data[rowNumber],
-    ltla21cd: columns[3].data[rowNumber],
-    lng: columns[4].data[rowNumber],
-    lat: columns[5].data[rowNumber],
-    population: columns[6].data[rowNumber]
+const decompressedData = decompressData(compressedData, (columnData, rowNumber) => ({
+    oa21cd: columnData[0][rowNumber],
+    lsoa21cd: columnData[1][rowNumber],
+    msoa21cd: columnData[2][rowNumber],
+    ltla21cd: columnData[3][rowNumber],
+    lng: columnData[4][rowNumber],
+    lat: columnData[5][rowNumber],
+    population: columnData[6][rowNumber]
 }));
 
 const endTime = performance.now();
